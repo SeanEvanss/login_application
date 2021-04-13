79284ecbb7987193bfe10dddc39dfa44fb9c00f3
NicChoo96 <nic_choo_96@hotmail.com>
2021-04-13 19:49:45 +0800

    Hashing Feature: - Updated SQL Query for hashed password - Added Login Model Hashing of password for security

--- src/main/java/org/example/LoginModel.java
+++ src/main/java/org/example/LoginModel.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class LoginModel {
@@ -9,7 +11,8 @@ public class LoginModel {
     public LoginModel(String username, String password)
     {
         this.username = username;
-        this.password = password;
+        this.password = hashPassword(password);
+        System.out.println(this.password);
     }
 
     public String getUsername() {
@@ -33,5 +36,37 @@ public class LoginModel {
         LoginValidator loginValidator = new LoginValidator();
 
         return loginValidator.queryLoginDatabase(username, password);
+    }
+
+    // Hash password before validating in the database
+    public String hashPassword(String inputPassword)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashedPassword = md.digest(inputPassword.getBytes(StandardCharsets.UTF_8));
+            return toHexString(hashedPassword);
+        }catch(Exception ex){
+            System.out.println("HERE ERROR:" + ex);
+        }
+        return "";
+    }
+    // Convert Hex to String
+    public static String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 32)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
     }
 }
--- user_authentication_db.sql
+++ user_authentication_db.sql
@@ -7,11 +7,12 @@ drop table if exists user_auth_data;
 create table user_auth_data(
 	id int auto_increment,
     userName varchar(50),
-    userPassword varchar(50),
+    userPassword varchar(200),
     primary key(id)
 );
 
 insert into user_auth_data(userName, userPassword)
-values ('sean', '12345');
+values ('sean', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
 
-select userName, userPassword from user_auth_data;
+-- update user_auth_data set userPassword = '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5' where id = 1;
+-- select userName, userPassword from user_auth_data;
